257e3f23ff5536faa9c339f9981fcad60952d96e
EKhan <EKhan15.sg@gmail.com>
2014-11-11 10:06:19 +0000

    Voting

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Bloccit::Application.routes.draw do
   resources :topics do
     resources :posts, except: [:index]
     
-  resources :posts do
+  resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
+
+    post '/up-vote' => 'votes#up_vote', as: :up_vote
+    post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
 end
 
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Vote do
   describe "validations" do
-    describe "valude validation" do
+    describe "value validation" do
       it "only allows -1 or 1 as values" do
         expect( v.valid ).to eq( true )
         expect( v2.valid ).to eq( true )
